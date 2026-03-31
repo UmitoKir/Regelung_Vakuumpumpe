10db28bdd47cde59aefbdc3ca653b8af36a198a4
UmitoKir <umito.kirchwehm@tum.de>
2026-03-31 13:24:47 +0200

    v2 31032026

--- regelung_vakuumpumpe/labortest5.py
+++ regelung_vakuumpumpe/labortest5.py
@@ -2,13 +2,14 @@
 #install all the necessary libaries and debugger/compiler(python, python debugger, git etc.) to get it running on VS code
 #install with py -m pip install in the terminal all the necessary packages
 #matplotlib maybe also required
+
 import time
 import numpy as np
 import nidaqmx
 import serial
 import serial.tools.list_ports
 import matplotlib.pyplot as plt
-from collections import deque
+import csv
 
 #der folgende Absatz sucht den usb port aus an dem sie die Vakuumpumpe aneschlossen haben
 ports = list(serial.tools.list_ports.comports()) #ruft eine Liste mit allen existierenden Anschlüssen an Ihrem Computer ab
@@ -33,7 +34,7 @@ Ableitung = []
 zeit = []
 Ventilspannung_Durchlass = []
 Ventilspannung_Einlass = []
-Dauer = 300.0
+Dauer = 1800.0
 
 ventilspannungen1 = [10, 9.5, 9, 8.5, 8, 7.5, 7, 6.5, 6, 5.5, 5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0.5, 0]
 ventilspannungen2 = [0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
@@ -46,6 +47,8 @@ test_konfigurationen = [
 
 untere_hystere = False
 obere_hystere = False
+
+csv_buffer = []
 
 def getpressure(ser): #"Druckauslesebefehl"
     response = ser.readline().decode('utf-8').strip() #liest die Werte vom CenterThree
@@ -60,14 +63,14 @@ def getpressure(ser): #"Druckauslesebefehl"
         return None
 
 
-def Druck_abfahren(ser,task, dt, wahl, ventilspannung, Startzeit, Startzeit_neuer_Druck, lokale_zeit):
+def Druck_abfahren(ser,task, dt, wahl, ventilspannung, Startzeit, Startzeit_neuer_Druck, lokale_zeit, filename):
         global old_pressure
         global Dauer
         global zeit, Druck, Ventilspannung_Durchlass, Ventilspannung_Einlass
         Endzeit = Startzeit_neuer_Druck + Dauer
         tangent_counter = 0
         compare_pressure = 0.0
-
+        global csv_buffer
         
         if wahl == "Einlass":
             task.write([10, ventilspannung])
@@ -76,7 +79,7 @@ def Druck_abfahren(ser,task, dt, wahl, ventilspannung, Startzeit, Startzeit_neue
             task.write([ventilspannung, 0])
             v_durch, v_ein = ventilspannung, 0
 
-        while(lokale_zeit < Endzeit):
+        while(tangent_counter < 100) and (lokale_zeit < Endzeit):
             pressure = getpressure(ser)
             while (pressure is None):
                     print("warte auf Druckwerte...")
@@ -88,6 +91,9 @@ def Druck_abfahren(ser,task, dt, wahl, ventilspannung, Startzeit, Startzeit_neue
             Ventilspannung_Einlass.append(v_ein)
             Druck.append(istWert)
             zeit.append(lokale_zeit)
+
+           
+            
             schwankung = (istWert - old_pressure)/old_pressure
             schwankung_in_relation_zum_vergleich = (istWert - compare_pressure)/compare_pressure if compare_pressure != 0 else 1
             
@@ -96,20 +102,38 @@ def Druck_abfahren(ser,task, dt, wahl, ventilspannung, Startzeit, Startzeit_neue
             elif (abs(schwankung) > 0.001 or abs(schwankung_in_relation_zum_vergleich) > 0.005) and tangent_counter >0:
                 tangent_counter = 0
                 compare_pressure = istWert
-            elif tangent_counter >= 100:  # Wenn die Ableitung wieder klein ist, aber vorher groß war
-                tangent_counter = 0
-                break
+            
+            druckeinstelldauer = lokale_zeit - Startzeit_neuer_Druck
+
+            if tangent_counter >= 100:
+                aktuelle_zeile =[f"{lokale_zeit: .3f}", f"{istWert: .5f}", f"{v_durch: .2f}", f"{v_ein: .2f}", wahl, f"{druckeinstelldauer: .3f}"]
+            else :
+                aktuelle_zeile =[f"{lokale_zeit: .3f}", f"{istWert: .5f}", f"{v_durch: .2f}", f"{v_ein: .2f}", wahl, ""]
+            
+            csv_buffer.append(aktuelle_zeile)
+
+            try: 
+                with open (filename, mode='a', newline='') as f:
+                    writer = csv.writer(f, delimiter=';')
+                    while csv_buffer:
+                        writer.writerow(csv_buffer[0])
+                        csv_buffer.pop(0)
+            except PermissionError :
+                print("Fehler: CSV Datei konnte nicht geöffnet werden. (Datei offen?)")
+                pass
+            
             old_pressure = istWert
-            print(f"Druck: {istWert:.5f} mBar | Einlassventil: {ventilspannung:.2f} V ")
+            print(f"Wahl des Ventils: {wahl} | Ventil: {ventilspannung:.2f} V | Druck: {istWert:.5f} mBar | Dauer der Stufe: {druckeinstelldauer:.3f} s | Tangent Counter: {tangent_counter}")
             
             while time.time()-Startzeit-lokale_zeit < dt:
                 time.sleep(0.005)
             lokale_zeit = time.time() - Startzeit
 
+
 def sensorwahl_mit_hysterese(pressure):
     global untere_hystere, obere_hystere, old_pressure
 
-    istWert = 0.0
+    istWert = old_pressure
 
     if pressure[0]>= 1.0: # ab >= 1mBar immer sensor 1 verwenden
         istWert = pressure[0] 
@@ -134,7 +158,7 @@ def sensorwahl_mit_hysterese(pressure):
         istWert = pressure[0]
         print("Sensor HP")
     if istWert < 0:
-        istWert = 0.0
+        istWert = old_pressure
     return istWert
 
        
@@ -144,6 +168,11 @@ def sensorwahl_mit_hysterese(pressure):
 def main():
     global ventilspannungen1, ventilspannungen2
     global test_konfigurationen
+    filename = f"messung_{time.strftime('%Y%m%d-%H%M%S')}.csv"
+    with open(filename, mode='w', newline='') as f:
+        writer = csv.writer(f, delimiter=';')
+        writer.writerow(['Zeit_s', 'Druck_mBar', 'V_Durchlass', 'V_Einlass', 'Modus', 'Dauer bis Druckstabilität_s'])
+
     try:
         ser = serial.Serial(port=sp, baudrate=br, timeout=to) #stellt Verbindung mit der Vakuumpumpe her (öffnet Chanel)
         print(f'Verbindung hergestellt mit {sp}')
@@ -161,17 +190,13 @@ def main():
             #task.write([0, 7.25])
             #time.sleep(5)
             #task.write([0, 10])
-
            
             Startzeit = time.time()
 
             for wahl, spannungs_liste in test_konfigurationen:
                 for spannung in spannungs_liste[:]:
                     Startzeit_neuer_Druck = time.time() - Startzeit
-                    Druck_abfahren(ser,task, dt, wahl, spannung, Startzeit, Startzeit_neuer_Druck, Startzeit_neuer_Druck)
-
-                      
-
+                    Druck_abfahren(ser,task, dt, wahl, spannung, Startzeit, Startzeit_neuer_Druck, Startzeit_neuer_Druck, filename)
             task.stop()
         #input("Enter drücken zum Beenden...")
     except KeyboardInterrupt:
@@ -181,13 +206,16 @@ def main():
     except UnicodeDecodeError as e:
         print(f'Fehler bei der Dekodierung: {e}')
     finally:
-        try: 
-            task.write([0.0, 0.0])  # Alle Ausgänge auf 0 setzen
+        try:
+            if 'task' in locals(): 
+                task.write([0.0, 0.0])  # Alle Ausgänge auf 0 setzen
+                task.stop()
             if 'ser' in locals() and ser.is_open:
                 ser.close()
                 print('Verbindung closed. ')
-        except Exception as e:
+        except Exception:
             pass
+        
     for i in range(len(Druck)-1):
         tangente = (Druck[i+1] - Druck[i]) / (zeit[i+1] - zeit[i])
         Ableitung.append(tangente)
